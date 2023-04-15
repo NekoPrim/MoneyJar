bd91c98accd921dbb30af8dbdac85181665b2853
Desiree Quade <nekodez@gmail.com>
2023-04-14 20:09:19 -0500

    starting header

---
+++ src/Components/Reusable/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.png';
+
+export default function Header() {
+    const standardMenu = [
+        
+    ];
+    
+    return (
+        <div>
+            <button>
+                <img src={logo} />
+            </button>
+        </div>
+    );
+}
--- src/Components/Screens/Categories/CategoriesScreen.tsx
+++ src/Components/Screens/Categories/CategoriesScreen.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from '../../Reusable/Header';
 
 export default function CategoriesScreen() {
     return (
+        <div>
         <h1>
             Categories Screen
         </h1>
+        <Header>
+            <div></div>
+        </Header>
+        </div>
     );
 }
